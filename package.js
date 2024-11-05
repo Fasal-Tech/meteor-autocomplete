481d2c15827da123487af2fab4d1eb4f41913968
MrSpark2591 <mr.spark2591@gmail.com>
2024-11-05 15:35:26 +0530

    version bumping

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: "mizzao:autocomplete",
+  name: "mrspark:autocomplete",
   summary: "Client/server autocompletion designed for Meteor's collections and reactivity",
-  version: "0.5.1",
+  version: "1.0.0",
   git: "https://github.com/mizzao/meteor-autocomplete.git"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("1.0");
+  api.versionsFrom("3.0.4");
 
-  api.use(['blaze', 'templating', 'jquery', 'check', 'tracker'], 'client');
+  api.use(['blaze@3.0.0', 'templating@1.4.4', 'jquery@1.11.11', 'check', 'tracker'], 'client');
   api.use(['coffeescript@1.0.0 || 2.0.0', 'underscore']); // both
   api.use(['mongo', 'ddp']);
 
@@ -31,7 +31,7 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function(api) {
-  api.use("mizzao:autocomplete");
+  api.use("mrspark:autocomplete");
 
   api.use('coffeescript@2.4.1');
   api.use('mongo');
